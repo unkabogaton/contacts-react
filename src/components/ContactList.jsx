ae9f920622967bbc495464aba1456cc6c7bee2f5
unkabogaton <anthonralph2@gmail.com>
2023-08-18 01:35:20 +0800

    Rendered 'contacts' array of objects in ContactList component

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -64,37 +64,40 @@ function ContactList() {
             role="list"
             className="divide-y divide-gray-200 dark:divide-gray-700"
           >
-            <li className="py-3 sm:py-4">
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0">
-                  <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                    <svg
-                      className="absolute w-12 h-12 text-gray-400 -left-1"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
+            {contacts.map((contact, index) => (
+              <li className="py-3 sm:py-4" key={index}>
+                <div className="flex items-center space-x-4">
+                  <div className="flex-shrink-0">
+                    <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                      <svg
+                        className="absolute w-12 h-12 text-gray-400 -left-1"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="flex-1 min-w-0">
+                    <p className="text-md font-medium text-gray-900 truncate dark:text-white">
+                      {contact.firstName} {contact.middleName}{" "}
+                      {contact.lastName}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      Email: {contact.mobileNumber}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      Number: {contact.emailAddress}
+                    </p>
                   </div>
                 </div>
-                <div className="flex-1 min-w-0">
-                  <p className="text-md font-medium text-gray-900 truncate dark:text-white">
-                    Neil Sims
-                  </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                    Email: email@windster.com
-                  </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                    Number: 09214748564
-                  </p>
-                </div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
