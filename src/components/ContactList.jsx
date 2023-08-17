d1e47c516ba299189a10ac0c881a5d4fd915faec
unkabogaton <anthonralph2@gmail.com>
2023-08-18 01:26:55 +0800

    Created Contacts Card List UI

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,10 +1,53 @@
 function ContactList() {
-    return (
-      <>
-       <h1>Contact List</h1>
-      </>
-    )
-  }
-  
-  export default ContactList
-  
+  return (
+    <>
+      <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto">
+        <div className="flex items-center justify-between mb-4">
+          <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+            Contacts List
+          </h5>
+        </div>
+        <div className="flow-root">
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 dark:divide-gray-700"
+          >
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                  <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                    <svg
+                      className="absolute w-12 h-12 text-gray-400 -left-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div className="flex-1 min-w-0">
+                  <p className="text-md font-medium text-gray-900 truncate dark:text-white">
+                    Neil Sims
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    Email: email@windster.com
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    Number: 09214748564
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContactList;
