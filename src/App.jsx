c40e6a9a16cc75f4cbcb7d0602d300d5e465ea22
unkabogaton <anthonralph2@gmail.com>
2023-08-18 01:27:20 +0800

    Centered items

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import ContactList from "./components/ContactList";
 function App() {
   return (
     <>
-     <ContactList />
+      <div className="container mx-auto py-10 px-5">
+        <ContactList />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
