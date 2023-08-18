2b977eb711ec1ab4b0536db7644a76b6175113ae
unkabogaton <anthonralph2@gmail.com>
2023-08-18 17:03:20 +0800

    Fixed the redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import contactReducer from "./redux/reducers";
+
+const store = createStore(contactReducer);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import contactReducer from "./reducers";
-
-const store = createStore(contactReducer);
-
-export default store;
