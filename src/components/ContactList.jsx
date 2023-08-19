5d9199963290a36fee06a31ad4861e2d3d08218d
unkabogaton <anthonralph2@gmail.com>
2023-08-19 15:25:01 +0800

    Added confirmation before deleting contact

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -63,7 +63,9 @@ function ContactList() {
                   <button
                     className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
                     onClick={() => {
-                      dispatch(deleteContact(contact.id));
+                      confirm(
+                        `Are you sure you want to delete this contact '${contact.firstName}'?`
+                      ) && dispatch(deleteContact(contact.id));
                     }}
                   >
                     Delete
